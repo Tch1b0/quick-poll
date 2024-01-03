10c75ea105be55d5f27777b2f77d4a625096082e
Tch1b0 <Jpour3006@gmail.com>
2024-01-03 18:03:51 +0100

    add secure and insecure ws

--- client/lib/connection.js
+++ client/lib/connection.js
@@ -1,9 +1,9 @@
 import { IS_DEBUG } from "./environment.js";
 
-const PROD_API_HOST = "qp-api.johannespour.de";
-const DEBUG_API_HOST = `${window.location.hostname}:8080`;
+const PROD_API_HOST = "wss://qp-api.johannespour.de";
+const DEBUG_API_HOST = `ws://${window.location.hostname}:8080`;
 
-const BASE_URL = `ws://${IS_DEBUG ? DEBUG_API_HOST : PROD_API_HOST}`;
+const BASE_URL = IS_DEBUG ? DEBUG_API_HOST : PROD_API_HOST;
 
 /**
  *
