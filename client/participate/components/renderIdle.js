3cf59c7104cdde7245447738e91bfeab53f5784f
Tch1b0 <Jpour3006@gmail.com>
2024-01-12 20:53:01 +0100

    fix chart width

--- client/participate/components/renderIdle.js
+++ client/participate/components/renderIdle.js
@@ -1,7 +1,7 @@
 export default function () {
     return `
         <div style="width: 100%;" class="center">
-            <h1>Poll starting soon!</h1>
+            <h1>Umfrage startet gleich!</h1>
             <div class="center ping">
                 <div class="p1"></div>
                 <div class="p2"></div>
