dceddbd98df747536440d16a0d8ea7a5f3a9e1dc
Tch1b0 <Jpour3006@gmail.com>
2024-01-16 19:51:19 +0100

    fix docker-compose

--- client/lib/environment.js
+++ client/lib/environment.js
@@ -1,4 +1,4 @@
-// assign constant IS_DEBUG to immediate evaluation of the hostname
+// assign constant IS_DEBUG to immediate evaluation of origin
 export const IS_DEBUG = (() => {
     const origin = window.location.hostname;
     const locals = ["localhost", "127.0.0.1", "::1", "0.0.0.0"];
--- client/participate/components/renderIdle.js
+++ client/participate/components/renderIdle.js
@@ -1,6 +1,6 @@
 export default function () {
     return `
-        <div style="width: 100%;" class="center">
+        <div class="center">
             <h1>Umfrage startet gleich!</h1>
             <div class="center ping">
                 <div class="p1"></div>
